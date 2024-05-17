c84028eaa30c6aeda17c37a9290d5d1db7ceb43b
gulserenzirek <gulserenzirek62@gmail.com>
2024-05-17 22:59:39 +0300

    added about_app and notifications

--- flutterfinalproje/lib/screens/settings_screens/about_app.dart
+++ flutterfinalproje/lib/screens/settings_screens/about_app.dart
@@ -1,11 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutterfinalproje/widgets/myappbar.dart';
+import 'package:gap/gap.dart';
 //import 'package:flutterfinalproje/widgets/mybottomnavbar.dart';
 
 import '../../core/localizations.dart';
 
 class AboutAppScreen extends StatefulWidget {
-  const AboutAppScreen({super.key});
+  const AboutAppScreen({Key? key}) : super(key: key);
 
   @override
   State<AboutAppScreen> createState() => _AboutAppScreenState();
@@ -18,10 +21,116 @@ class _AboutAppScreenState extends State<AboutAppScreen> {
       appBar: MyAppBar(
         title: AppLocalizations.of(context).getTranslate("about_app"),
       ),
-      body: const SafeArea(
-        child: Text("Uygulama Hakkında Sayfası")
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Gap(20),
+              Center(
+                child: Image.asset(
+                  "assets/images/logo/GR_Logo.png",
+                  height: 150,
+                  width: 150,
+                ),
+              ),
+              Center(
+                child: Text(
+                  AppLocalizations.of(context).getTranslate("app_name"),
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Divider(),
+              const Gap(20),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildSectionTitle(
+                        AppLocalizations.of(context)
+                            .getTranslate("general_description"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("explanation_of_general_definition"),
+                      ),
+                      const Gap(20),
+                      _buildSectionTitle(
+                        AppLocalizations.of(context)
+                            .getTranslate("about_app_title_1"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("description_about_app_1"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate(" description_about_app_2"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("description_about_app_3"),
+                      ),
+                      const Gap(20),
+                      _buildSectionTitle(
+                        AppLocalizations.of(context)
+                            .getTranslate("about_app_title_2"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("description_about_app_4"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("description_about_app_5"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("description_about_app_6"),
+                      ),
+                      const Gap(20),
+                      _buildSectionTitle(
+                        AppLocalizations.of(context)
+                            .getTranslate("about_app_title_3"),
+                      ),
+                      _buildListItemWithIcon(
+                        AppLocalizations.of(context)
+                            .getTranslate("description_about_app_7"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       //bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    );
+  }
+
+  Widget _buildListItemWithIcon(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "• ",
+            style: TextStyle(fontSize: 20),
+          ),
+          Expanded(child: Text(text)),
+        ],
+      ),
     );
   }
 }
--- flutterfinalproje/lib/screens/settings_screens/notifications.dart
+++ flutterfinalproje/lib/screens/settings_screens/notifications.dart
@@ -1,27 +1,211 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../core/localizations.dart';
 import '../../widgets/myappbar.dart';
-//import '../../widgets/mybottomnavbar.dart';
 
 class NotificationsScreen extends StatefulWidget {
-  const NotificationsScreen({super.key});
+  const NotificationsScreen({Key? key}) : super(key: key);
 
   @override
   State<NotificationsScreen> createState() => _NotificationsScreenState();
 }
 
 class _NotificationsScreenState extends State<NotificationsScreen> {
+  List<Map<String, dynamic>> notifications = [
+    {
+      "title": "",
+      "description": "",
+      "enabled": true,
+    },
+    {
+      "title": "",
+      "description": "",
+      "enabled": true,
+    },
+    {
+      "title": "",
+      "description": "",
+      "enabled": true,
+    },
+    {
+      "title": "",
+      "description": "",
+      "enabled": true,
+    },
+    {
+      "title": "",
+      "description": "",
+      "enabled": true,
+    },
+  ];
+
+  int selectedOption = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPreferences(); // Önbellekten tercihleri yükle
+  }
+
+// Önbellekten tercihleri yükler
+  _loadPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      notifications = notifications.map((notification) {
+        notification["title"] = AppLocalizations.of(context).getTranslate(
+            "notifications_title${notifications.indexOf(notification) + 1}");
+        notification["description"] = AppLocalizations.of(context).getTranslate(
+            "notifications_description${notifications.indexOf(notification) + 1}");
+        bool? enabled = prefs.getBool(notification['title']);
+        if (enabled != null) {
+          notification['enabled'] = enabled;
+        }
+        return notification;
+      }).toList();
+      selectedOption = prefs.getInt('selected_option') ?? 0;
+    });
+  }
+
+// Tercihleri kaydeder
+  _savePreference(String key, dynamic value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (value is bool) {
+      prefs.setBool(key, value);
+    } else if (value is int) {
+      prefs.setInt(key, value);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(
         title: AppLocalizations.of(context).getTranslate("notifications"),
       ),
-      body: const SafeArea(
-        child: Text("Bildirimler Sayfası")
+      body: SafeArea(
+        child: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: notifications.length + 1,
+          itemBuilder: (context, index) {
+            if (index == notifications.length) {
+              // Tercih seçenekleri listesi
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gap(10),
+                  Divider(),
+                  Gap(10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      AppLocalizations.of(context)
+                          .getTranslate("notifications_inform_me"),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      AppLocalizations.of(context)
+                          .getTranslate("all_new_messages"),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      AppLocalizations.of(context)
+                          .getTranslate("all_new_messages_description"),
+                    ),
+                    leading: Radio(
+                      value: notifications.length,
+                      groupValue: selectedOption,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedOption = value as int;
+                        });
+                        _savePreference('selected_option', value);
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      AppLocalizations.of(context)
+                          .getTranslate("mentioned_only"),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      AppLocalizations.of(context)
+                          .getTranslate("mentioned_only_description"),
+                    ),
+                    leading: Radio(
+                      value: notifications.length + 1,
+                      groupValue: selectedOption,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedOption = value as int;
+                        });
+                        _savePreference('selected_option', value);
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      AppLocalizations.of(context).getTranslate("nothing"),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      AppLocalizations.of(context)
+                          .getTranslate("nothing_description"),
+                    ),
+                    leading: Radio(
+                      value: notifications.length + 2,
+                      groupValue: selectedOption,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedOption = value as int;
+                        });
+                        _savePreference('selected_option', value);
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }
+            // Bildirim kartı
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ListTile(
+                title: Text(
+                  notifications[index]['title']!,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(notifications[index]['description']!),
+                leading: Icon(Icons.notifications),
+                trailing: Switch(
+                  value: notifications[index]['enabled'],
+                  onChanged: (bool value) {
+                    setState(() {
+                      notifications[index]['enabled'] = value;
+                    });
+                    _savePreference(notifications[index]['title']!, value);
+                  },
+                ),
+              ),
+            );
+          },
+        ),
       ),
-      //bottomNavigationBar: MyBottomNavBar(),
     );
   }
 }
